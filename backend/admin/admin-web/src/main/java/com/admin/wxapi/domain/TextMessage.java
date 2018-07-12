044be948185307136adfc1c153d2dd3b235f3205
fengchaseyou <fengchaseyou@163.com>
2018-07-12 20:16:19 +0800

    优化

--- backend/admin/admin-web/src/main/java/com/admin/wxapi/domain/TextMessage.java
+++ backend/admin/admin-web/src/main/java/com/admin/wxapi/domain/TextMessage.java
@@ -5,11 +5,38 @@ package com.admin.wxapi.domain;
 文件：TextMessage.java
 项目：admin-web
 */
-public class TextMessage extends BaseMessage{
+public class TextMessage {
 
     private String Content;
     private String MsgId;
-
+    private String ToUserName;
+    private String FromUserName;
+    private long CreateTime;
+    private String MsgType;
+    public String getToUserName() {
+        return ToUserName;
+    }
+    public void setToUserName(String toUserName) {
+        ToUserName = toUserName;
+    }
+    public String getFromUserName() {
+        return FromUserName;
+    }
+    public void setFromUserName(String fromUserName) {
+        FromUserName = fromUserName;
+    }
+    public long getCreateTime() {
+        return CreateTime;
+    }
+    public void setCreateTime(long createTime) {
+        CreateTime = createTime;
+    }
+    public String getMsgType() {
+        return MsgType;
+    }
+    public void setMsgType(String msgType) {
+        MsgType = msgType;
+    }
     public String getContent() {
         return Content;
     }
